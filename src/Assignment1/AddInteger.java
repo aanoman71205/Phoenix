5a2d2ec854d6bf7aad6a752f632c122b145ad055
Abdullah Al Noman <Abdullah Al Noman@10.67.225.57>
2021-09-04 15:07:26 -0400

    my 1st commit

---
+++ src/Assignment1/AddInteger.java
@@ -0,0 +1,15 @@
+package Assignment1;
+
+public class AddInteger {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		x = 25;
+		y = 35;
+		int sum = x + y;
+		System.out.println("The Total is: " + sum);
+
+	}// end of main
+
+}// end of class
